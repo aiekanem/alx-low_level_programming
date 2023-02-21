609904a9b279bfd25a58934a8a5cc6b2862e4b67
aiekanem <wb.basky@outlook.com>
2023-02-21 07:34:24 -0800

    task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar
  * Return: 0 if succesful
  */
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
