f71fd02b794a21571284238023735c6a3ba66200
aiekanem <wb.basky@outlook.com>
2023-03-22 13:01:24 -0700

    task 3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,29 +1,62 @@
 #include "3-calc.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
- * get_op_func - selects the correct function to perform
- * @s: operator passed as argument
- * Return: pointer to function
+ * op_add - sum of a and b
+ * @a: integer to add
+ * @b: integer to add
+ * Return: sum if succesful
  */
 
-int (*get_op_func(char *s))(int a, int b)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (ops[i].op)
+	return ((a) + (b));
+}
+/**
+ * op_sub - difference of a and b
+ * @a: integer to subtract
+ * @b: integer to subtract
+ * Return: difference if succesful
+ */
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+/**
+ * op_mul - multiply a and b
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: multiplication of a and b if succesful
+ */
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+/**
+ * op_div - divides a and b
+ * @a: integer to divide
+ * @b: integer to divide
+ * Return: division if succesful
+ */
+int op_div(int a, int b)
+{
+	if (!b)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (ops[i].f);
+	return ((a) / (b));
+}
+/**
+ * op_mod - Module of a and b
+ * @a: integer
+ * @b: integer
+ * Return: remainder if succesful
+ */
+int op_mod(int a, int b)
+{
+	if (b)
+		return ((a) % (b));
+	printf("Error\n");
+	exit(100);
 }
